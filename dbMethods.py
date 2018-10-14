35338f8f4fa848fd8b660246790b078ef2327eaf
Michael <sheveksmath@gmail.com>
2018-10-13 23:32:38 -0400

    Initial commit

---
+++ dbMethods.py
@@ -0,0 +1,242 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from initDB import Recipe, Ingredient, RecIng, MealPlanItem, ShoppingListItem, Inventory, InventoryTransactionHistory, Base
+
+engine = create_engine('sqlite:///pbdb.db')
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+# Misc
+
+def get_or_create(session, model, **kwargs):
+    try:
+        return session.query(model).filter_by(**kwargs).one(), True
+    except NoResultFound:
+        return model(**kwargs), False
+
+# CRUD Methods
+
+# Create
+def addRecipe(recipeName):
+    session = DBSession()
+    rec, exists = get_or_create(session, Recipe, name=recipeName.lower())
+    if exists != True:
+        try:
+            session.add(rec)
+            session.commit()
+            print("Insert success")
+        except Exception as e:
+            session.rollback()
+            print("Insert failed")
+            print(e)
+        finally:
+            session.close()
+    else:
+        print("Recipe already exists.")
+        return "Recipe already exists."
+        session.close()
+
+def addIngredient(ingredientName):
+    session = DBSession()
+    ing, exists = get_or_create(session, Ingredient, name=ingredientName.lower())
+    if exists != True:
+        try:
+            session.add(ing)
+            session.commit()
+            print("Insert success")
+        except Exception as e:
+            session.rollback()
+            print("Insert failed")
+            print(e)
+        finally:
+            session.close()
+    else:
+        print("Ingredient already exists.")
+        return "Ingredient already exists."
+        session.close()
+
+def addRecIng(recipe, ingredient, amount):
+    # adds entry to rec_ing table
+    # if the recipe, ingredient, and/or rec-ing association do not already exists,
+    # these are created and added to the appropriate table
+
+    session = DBSession()
+    rec, recExists = get_or_create(session, Recipe, name=recipe.lower())
+    ing, ingExists = get_or_create(session, Ingredient, name=ingredient.lower())
+
+    if recExists != True: #adds and commits recipe if it does not exists
+        session.add(rec) 
+        session.commit()
+
+    if ingExists != True: #adds and commits ingredient if it does not exists
+        session.add(ing)
+        session.commit()
+
+    # gets pre-existing rec_ing entry or creates one
+    assoc_table_entry, recIngExists = get_or_create(session, RecIng, rec_id=rec.id, ing_id=ing.id)
+    if recIngExists != True: # commits if it does not exist
+        assoc_table_entry.ingredient = ing
+        assoc_table_entry.amount = amount
+        rec.ingredients.append(assoc_table_entry)
+        try:
+            session.add(assoc_table_entry)
+            session.commit()
+        except:
+            session.rollback()
+        finally:
+            session.close()
+    else: # updates amount in rec_ing if it already exists in table
+        try:
+            assoc_table_entry.amount += amount
+            session.add(assoc_table_entry)
+            session.commit()
+        except:
+            session.rollback()
+        finally:
+            session.close()
+
+def addRecToPlan(recipe):
+    # adds recipe to mealplan table
+    # if recipe already in table, increases amount of rec by 1
+
+    session = DBSession()
+    rec, exists = get_or_create(session, Recipe, name=recipe.lower())
+    plan_item, exists2 = get_or_create(session, MealPlanItem, rec_id=rec.id, recipe=rec)
+    if exists2 != True:
+        plan_item.amount = 1
+    else:
+        plan_item.amount += 1
+
+    try: 
+        session.add(plan_item)
+        session.commit()
+        print("Recipe added to meal plan")
+    except:
+        session.rollback()
+        print("Recipe not added to meal plan")
+    finally:
+        session.close()
+
+def addPlanToList():
+    # adds ingredients for all recipes in mealplan to shoppinglist
+
+    session = DBSession()
+
+    # query for MealPlanItem, RecIng, & Ingredient tables
+    # pulls the RecIng & Ingredient records matching MealPlan rec id
+    q = session.query(MealPlanItem, RecIng, Ingredient).filter(MealPlanItem.rec_id == RecIng.rec_id).filter(Ingredient.id == RecIng.ing_id)
+    
+    for meal, ri, ing in q:
+        print("recipe id: " + str(meal.rec_id), "amount: " + str(meal.amount), "ing id:" + str(ing.id))
+        list_item, itemExists = get_or_create(session, ShoppingListItem, ing_id=ing.id, ingredient=ing)
+        if itemExists != True: # if item not already on shopping list, add it
+            try:
+                print("item not in list, added")
+                list_item.amount = ri.amount * meal.amount # list amount = # in recipe * # of recipes in meal plan
+                session.add(list_item)
+                session.commit()
+            except:
+                print("unable to add item to list")
+                session.rollback()
+        else:
+            try: # if item already in shopping list, update amount
+                print("item in list, amount updated")
+                list_item.amount += ri.amount * meal.amount # note: will need to subtract anything in inventory
+                session.add(list_item)
+                session.commit()
+            except:
+                print("unable to update amount of pre-existing shopping list item")
+                session.rollback()
+                
+    session.close()
+
+# def addListToInventory():
+
+# def addIngToInventory(ingredient):
+    # needs to add to both Inventory and InventoryTransationHistory
+
+
+# addRecIng("pizza", "cheese", 3)
+# addRecIng("pizza", "tomato sauce", 1)
+
+# addRecToPlan("pizza")
+
+# addPlanToList()
+
+# Read
+
+def view_all_ingredients():
+    # returns array of all ingredients in ingredients table
+    session = DBSession()
+    q = session.query(Ingredient)
+    ings = []
+    for ing in q:
+        ings += ing.name
+        print(ing.name)
+    return ings
+    session.close()
+
+def view_all_recipes():
+    # returns array of all ingredients in ingredients table
+    session = DBSession()
+    q = session.query(Recipe)
+    recs = []
+    for rec in q:
+        recs += rec.name
+        print(rec.name)
+    return recs
+    session.close()
+
+def view_recipe_ingredients(rec_name=None, rec_id=None):
+    # returns all ingredients from RecIng associated with recipe
+    # accepts recipe as string or by id
+    session = DBSession()
+    if rec_name != None:
+        q = session.query(RecIng, Recipe, Ingredient).filter(Recipe.name == rec_name).filter(Recipe.id == RecIng.rec_id).filter(Ingredient.id == RecIng.ing_id)
+    elif rec_id != None:
+        q = session.query(RecIng, Recipe, Ingredient).filter(Recipe.id == rec_id).filter(Recipe.id == RecIng.rec_id).filter(Ingredient.id == RecIng.ing_id)
+    else:
+        return None
+
+    for x in q:
+        print(x[2].name)
+
+    session.close()
+
+# def view_mealplan():
+
+# def view_shoppinglist():
+
+# def view_inventory():
+
+
+# Update
+
+# def change_recipe_name(recipeName, newRecipeName):
+
+# def change_recIng_amount(recipe, ingredient, newAmount):
+
+# def use_inv_item():
+    # deletes from inventory, but also adds inv transation
+
+
+
+# Delete
+
+# def del_recipe(recipe):
+
+# def del_ingredient(ingredient):
+
+# def del_recIng(recipe, ingredient):
+
+# def del_ing_from_shoppinglist(ingredient):
+
+# def del_rec_from_mealplan(rec):
+
+# def del_ing_from_inv(ing):
+    # does not keep transaction history of deletion; use in case of inv discrepancies
+
+
+
---
+++ initDB.py
@@ -0,0 +1,59 @@
+import os, sys
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Boolean
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class RecIng(Base):
+    __tablename__ = 'rec_ing'
+    id = Column(Integer, primary_key=True)
+    rec_id = Column(Integer, ForeignKey('recipes.id'))
+    ing_id = Column(Integer, ForeignKey('ingredients.id'))
+    amount = Column(Integer)
+    recipe = relationship("Recipe", back_populates="ingredients")
+    ingredient = relationship('Ingredient', back_populates='recipes')
+
+class Recipe(Base):
+    __tablename__ = 'recipes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    ingredients = relationship("RecIng", back_populates="recipe")
+
+class Ingredient(Base):
+    __tablename__ = 'ingredients'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    recipes = relationship("RecIng", back_populates='ingredient')
+
+class MealPlanItem(Base):
+    __tablename__ = 'mealplan'
+    id = Column(Integer, primary_key=True)
+    rec_id = Column(Integer, ForeignKey('recipes.id'))
+    amount = Column(Integer)
+    recipe = relationship('Recipe')
+
+class ShoppingListItem(Base):
+    __tablename__ = 'shoppinglist'
+    id = Column(Integer, primary_key=True)
+    ing_id = Column(Integer, ForeignKey('ingredients.id'))
+    amount = Column(Integer)
+    ingredient = relationship('Ingredient')
+
+class Inventory(Base):
+    __tablename__ = 'inventory'
+    ing_id = Column(Integer, ForeignKey('ingredients.id'), primary_key=True)
+    amount = Column(Integer)
+    ingredient = relationship('Ingredient')
+
+class InventoryTransactionHistory(Base):
+    __tablename__ = 'inv_transactions'
+    ing_id = Column(Integer, ForeignKey('ingredients.id'), primary_key=True)
+    added_or_subtracted = Column(Boolean, unique=False) 
+    # added = True, subtracted = False
+
+engine = create_engine('sqlite:///pbdb.db')
+
+Base.metadata.create_all(engine)
